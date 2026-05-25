85bb60038bc32a4d83b10920469ffee4147c67e3
ciarandagen-collab <ciarandagen@icloud.com>
2026-05-25 22:15:43 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gaa-stats-v14';
+const CACHE_NAME = 'gaa-stats-v16';
 const PRECACHE_URLS = ['./index.html','./manifest.json'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE_URLS)).then(() => self.skipWaiting()));
