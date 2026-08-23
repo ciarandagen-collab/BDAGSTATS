9232f2f98d62d8c6bce8cf35840dbcd9c94becaa
ciarandagen-collab <ciarandagen@icloud.com>
2026-08-23 19:45:29 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gaa-stats-v76';
+const CACHE_NAME = 'gaa-stats-v77';
 const PRECACHE_URLS = ['./app.html','./manifest.json'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE_URLS)).then(() => self.skipWaiting()));
